710445d06c9705813790db0d35ed29f64a00b5b8
EPozo07 <eloyempresa07gmail.com>
2026-05-18 09:30:11 +0200

    fix: mejorar responsive en móvil para home, generacion y favoritos

---
+++ android/app/src/main/java/com/eloy/pokedexapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.eloy.pokedexapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eloy.pokedexapp',
+  appName: 'pokedexApp',
+  webDir: 'dist/pokedex-app/browser'
+};
+
+export default config;
